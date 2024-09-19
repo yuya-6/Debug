b1c5df98cbb84688e39d4d7bb33357316f7bbbac
yuya-6 <yuchongakufu@gmail.com>
2024-09-19 21:50:41 +0900

    開始

---
+++ script.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+function toOneDimension( previousValue, currentValue ) {
+    return previousValue.concat( currentValue );
+}
+
+var sampleArrayA = [
+    [ 'A1', 'A2', 'A3' ],
+    [ 'B1', 'B2', 'B3' ],
+    [ 'C1', 'C2', 'C3' ]
+];
+
+var sampleArrayB = sampleArrayA.reduce( toOneDimension );
+
+for ( var counterVar = 0; counterVar < sampleArrayB.length; counterVar++ ) {
+    document.write( 'sampleArrayB[' +counterVar +']: ' );
+    document.write( sampleArrayB[counterVar] );
+    document.write( '<br />' );
+}
+</script>
